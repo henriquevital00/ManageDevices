ccc0555698b6a2817e7b97958a628d140a2006f8
henriquevital00 <henriquevital1000@hotmail.com>
2026-01-16 00:09:29 -0300

    Initialize Spring Boot app with Device schema migrations

---
+++ src/main/java/org/example/ManageDevices.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageDevices {
+    public static void main(String[] args) {
+        SpringApplication.run(ManageDevices.class, args);
+    }
+}
